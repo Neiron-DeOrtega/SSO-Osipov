8d9014b2a372df0d54c706f20ec080c7323493f6
Neiron-DeOrtega <igorpodelsk0@mail.ru>
2025-03-06 13:36:58 +0300

    not frontend files delete

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-import { initOAuth } from './oauth';
-import { initSAML } from './saml';
-import { Config } from './types';
-
-export function configureSSO(app: Express, config: Config): void {
-    initOAuth(app, config.oauth);
-    initSAML(app, config.saml);
-}
